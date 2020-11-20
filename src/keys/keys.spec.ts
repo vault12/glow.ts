0d2bdb3793a3ea3ce0b994e30349e0a1a72302bd
Pavlo Liapin <pavel.lyapin@gmail.com>
2020-11-20 12:50:59 +0200

    add Jest

---
+++ src/keys/keys.spec.ts
@@ -0,0 +1,31 @@
+/* jest.mock('../environment.ts', () => ({
+  IS_DEV: true,
+  IS_PROD: false,
+})); */
+
+import { Keys } from './keys';
+
+describe('Keys', () => {
+  let keys: Keys;
+
+  beforeEach(() => {
+    keys = new Keys('test');
+  });
+
+  it('should return base64', () => {
+    const actual = keys.toBase64();
+    const expected = 'dGVzdA==';
+
+    expect(actual).toBe(expected);
+  });
+
+  it('should return source', () => {
+    const spyWarn = jest.spyOn(console, 'log');
+    const input = 'd2hhdCBpcyBsb3Zl';
+    const actual = keys.fromBase64(input);
+    const expected = 'what is love';
+
+    expect(actual).toBe(expected);
+    expect(spyWarn).toHaveBeenCalledWith(`decrypting ${input}...`);
+  });
+});
