923e581c4b4bbdfb73a397856a270aaa19cb4640
Pavlo Liapin <pavel.lyapin@gmail.com>
2020-11-24 12:54:09 +0200

    add box

--- src/nacl/nacl.ts
+++ src/nacl/nacl.ts
@@ -1,4 +1,9 @@
-import { randomBytes, secretbox } from 'tweetnacl';
+import { randomBytes, secretbox, box, BoxKeyPair } from 'tweetnacl';
+
+interface CryptoBoxKeypair {
+  boxPk: Uint8Array
+  boxSk: Uint8Array
+}
 
 export class Nacl {
   random_bytes(size: number): Uint8Array {
@@ -16,5 +21,32 @@ export class Nacl {
   }
   crypto_secretbox_open(box: Uint8Array, nonce: Uint8Array, key: Uint8Array): Uint8Array | null {
     return secretbox.open(box, nonce, key);
+  }
+
+  crypto_box(message: Uint8Array, nonce: Uint8Array, pk: Uint8Array, sk: Uint8Array): Uint8Array {
+    return box(message, nonce, pk, sk);
+  }
+  crypto_box_open(ciphertext: Uint8Array, nonce: Uint8Array, pk: Uint8Array, sk: Uint8Array): Uint8Array {
+    return box.open(ciphertext, nonce, pk, sk);
+  }
+
+  crypto_box_random_nonce(): Uint8Array {
+    return randomBytes(box.nonceLength);
+  }
+
+  crypto_box_keypair(): CryptoBoxKeypair {
+    const pair: BoxKeyPair = box.keyPair();
+    return {
+      boxPk: pair.publicKey,
+      boxSk: pair.secretKey
+    };
+  }
+
+  crypto_box_keypair_from_raw_sk(key: Uint8Array): CryptoBoxKeypair {
+    const pair: BoxKeyPair = box.keyPair.fromSecretKey(key);
+    return {
+      boxPk: pair.publicKey,
+      boxSk: pair.secretKey
+    };
   }
 }
