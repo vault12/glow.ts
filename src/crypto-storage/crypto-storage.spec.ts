7114084ea2e66ea4304bf928a73f80526c5e5a63
Pavlo Liapin <pavel.lyapin@gmail.com>
2020-12-01 16:17:48 +0200

    better storage spec

--- src/crypto-storage/crypto-storage.spec.ts
+++ src/crypto-storage/crypto-storage.spec.ts
@@ -8,10 +8,32 @@ describe('CryptoStorage', () => {
     storage = new CryptoStorage(new LocalStorageDriver());
   });
 
-  it('save/read', async () => {
-    const object = { field1: 'string value', field2: 101 };
-    await storage.save('test', object);
-    const restored = await storage.get('test');
-    expect(JSON.stringify(object)).toBe(JSON.stringify(restored));
+  it('encrypted write/read', async () => {
+    const secretPlaintext = 'The quick brown fox jumps over the lazy dog';
+    const secretObject = {
+      field1: 'string value', // String
+      field2: 101, // Number
+      field3: { // Object
+        inside: 'secret'
+      },
+      field4: [1, 2, 3, 'big', 'secrets'], // Array
+      field5: 'Kæmi ný öxi hér ykist þjófum nú bæði víl og ádrepa' // Unicode string in Icelandic
+    };
+
+    await storage.save('secretPlaintext', secretPlaintext);
+    const restoredPlaintext = await storage.get('secretPlaintext');
+    expect(secretPlaintext).toBe(restoredPlaintext);
+
+    await storage.save('secretObject', secretObject);
+    const restoredObject = await storage.get('secretObject');
+    expect(JSON.stringify(secretObject)).toBe(JSON.stringify(restoredObject));
+
+    await storage.remove('secretPlaintext');
+    await storage.remove('secretObject');
+
+    const notRestoredPlaintext = await storage.get('secretPlaintext');
+    const notRestoredObject = await storage.get('secretObject');
+    expect(notRestoredPlaintext).toBeNull();
+    expect(notRestoredObject).toBeNull();
   });
 });
