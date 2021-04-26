4029aa247784d120f8c6fe03de0b7e69d7d1e14f
Pavlo Liapin <pavel.lyapin@gmail.com>
2021-04-26 17:45:19 +0300

    add mutex

--- src/relay/relays.service.ts
+++ src/relay/relays.service.ts
@@ -1,16 +1,20 @@
 import { Relay } from './relay';
+import { Mutex } from 'async-mutex';
 
 export class RelaysService {
   private static relays: {[url: string]: Relay} = {};
+  private static relayMutex = new Mutex();
 
   /**
    * Returns a Relay instance for given URL, or creates a new one if wasn't initialized
    */
   static async getRelay(url: string): Promise<Relay> {
-    let relay = this.relays[url];
-    if (!relay) {
-      relay = this.relays[url] = await Relay.new(url);
-    }
-    return relay;
+    return await this.relayMutex.runExclusive(async () => {
+      let relay = this.relays[url];
+      if (!relay) {
+        relay = this.relays[url] = await Relay.new(url);
+      }
+      return relay;
+    });
   }
 }
