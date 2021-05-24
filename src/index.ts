63a008906f33defc0dc975f36a8428e09ade0d8f
Pavlo Liapin <pavel.lyapin@gmail.com>
2021-05-24 18:34:59 +0300

    add build script for both cjs and es2020

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import { NaCl } from './nacl/nacl';
+import { Keys } from './keys/keys.js';
+import { KeyRing } from './keyring/keyring.js';
+import { CryptoStorage } from './crypto-storage/crypto-storage.js';
+import { StorageDriver } from './crypto-storage/storage-driver.interface.js';
+import { LocalStorageDriver } from './crypto-storage/local-storage.driver.js';
+import { Mailbox } from './mailbox/mailbox';
+import { Relay } from './relay/relay';
+
+export {
+  NaCl,
+  Keys,
+  KeyRing,
+  Mailbox,
+  Relay,
+  CryptoStorage,
+  StorageDriver,
+  LocalStorageDriver
+};
